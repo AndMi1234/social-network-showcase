98de4b91cff25e7fffc2b967bbd873b12c7ff543
andreas <mm258@uni-heidelberg.de>
2024-07-09 08:53:49 +0200

    frontend and backend first commit

---
+++ backend/src/main/java/com/andmi/socialNetworkShowcase/SocialNetworkShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.andmi.socialNetworkShowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkShowcaseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/andmi/socialNetworkShowcase/SocialNetworkShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andmi.socialNetworkShowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
